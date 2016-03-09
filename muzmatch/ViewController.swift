b5c79667a6180d642cf0ad2f1eb05232f9ec1450
Nabil Freeman <nabil@instafork.com>
2016-03-09 19:33:20 +0800

    Initial Commit

---
+++ muzmatch/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  muzmatch
+//
+//  Created by Nabil Freeman on 09/03/2016.
+//  Copyright © 2016 Nabil Freeman. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
